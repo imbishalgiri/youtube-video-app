f0eede524eaf8c312d64c5d350a8ed4c90a33ab8
bishal giri <giribishal09@gmail.com>
2020-02-20 21:08:10 +0545

    deployment file

---
+++ build/precache-manifest.01a975c5876e0fc6aebb837b97e552b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5643fdf9619d9108dc9ef241e14635f3",
+    "url": "/youtube-video-apps/index.html"
+  },
+  {
+    "revision": "ecd3ea50940818d88002",
+    "url": "/youtube-video-apps/static/css/main.6661f085.chunk.css"
+  },
+  {
+    "revision": "836a5b19cfa526c00a5c",
+    "url": "/youtube-video-apps/static/js/2.be3b7309.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/youtube-video-apps/static/js/2.be3b7309.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecd3ea50940818d88002",
+    "url": "/youtube-video-apps/static/js/main.fe9f4d03.chunk.js"
+  },
+  {
+    "revision": "b1c1e009659d43429689",
+    "url": "/youtube-video-apps/static/js/runtime-main.5fdc08cf.js"
+  }
+]);
